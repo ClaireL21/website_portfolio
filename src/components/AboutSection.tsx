fb0557b2f34276498e5fcd4641481965cf14ab94
ClaireL21 <53claire@gmail.com>
2025-01-04 16:42:27 -0500

    trying img not Image

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import Image from 'next/image'
+//import Image from 'next/image'
 //import headshot from "/headshot_rect_small.jpg"
 import { HiArrowDown } from "react-icons/hi"
 import { Link as JumpAnchor} from "react-scroll/modules"
@@ -15,7 +15,7 @@ const AboutSection = () => {
         </h1>
         <div className="flex flex-col text-center items-center justify-center mt-10 px-10 lg:px-40 md:flex-row md:space-x-8 md:text-left">
           <div className="md:w-2/5">
-            <Image className="" src="/headshot_rect_small.JPG" alt="" width={400} height={400}/>
+            <img className="" src="/headshot_rect_small.jpg" alt="" width={400} height={400}/>
           </div>
           <div className="md:w-1/2 md:mt-2">
             <h1 className="font-bold text-3xl mt-6 lg:mt-0">Hi, I'm Claire!</h1>
